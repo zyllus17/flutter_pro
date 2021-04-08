911e8b50b332ab11194dbb43af3b7894cc74a7e4
Maruf Hassan <mrfhassan17@gmail.com>
2021-04-09 01:30:42 +0530

    example test

---
+++ test/example_test.dart
@@ -0,0 +1,11 @@
+//An example test to see if the [launch.json] is working properly or not
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    "1 + 1 == 2",
+    () async {
+      expect(1 + 1 == 2, true);
+    },
+  );
+}
