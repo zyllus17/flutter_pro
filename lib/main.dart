16ccf39ae72ffc35e82a848484ae79537a261514
Maruf Hassan <mrfhassan17@gmail.com>
2021-04-10 00:55:48 +0530

    Shows title according to flavor

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pro/core/app_config.dart';
 import 'package:flutter_pro/core/flutter_pro.dart';
 
 class MyApp extends StatelessWidget {
   final Widget screen;
   MyApp(this.screen);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'FlutterPro App',
+      title: AppConfig.of(context).appTitle,
       home: FlutterPro(),
     );
   }
