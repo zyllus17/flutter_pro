39f1b40e7a180b083cc35cd65fcb66ebabfcddd7
Maruf Hassan <mrfhassan17@gmail.com>
2021-04-10 00:56:02 +0530

    Shows AppBar according to Flavor

--- lib/core/flutter_pro.dart
+++ lib/core/flutter_pro.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app_config.dart';
+
 //This is the widget below the [MyApp] root widget
 class FlutterPro extends StatelessWidget {
   const FlutterPro({
@@ -10,11 +12,13 @@ class FlutterPro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FlutterPro App Bar'),
+        title: Text(
+          AppConfig.of(context).appTitle,
+        ),
       ),
       body: Center(
         child: Container(
-          child: Text('FlutterPro'),
+          child: Text('You are running ${AppConfig.of(context).appTitle}'),
         ),
       ),
     );
